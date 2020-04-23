db59e235b9e27f833e80f371bc4124e2aadd92e9
Dalil <dalil.tadjeddine@ens.uvsq.fr>
2020-04-23 14:16:49 +0200

    Create CommandRpn.java

---
+++ src/main/java/fr/uvsq21914172/pglp42/commands/CommandRpn.java
@@ -0,0 +1,16 @@
+package fr.uvsq21914172.pglp42.commands;
+
+/**
+ * Interface presentant les commandes pouvant etre interprete par le moteur.
+ *
+ * @author Dalil
+ *
+ */
+public interface CommandRpn {
+
+  /**
+   * Methode contenant l'execution de la commande.
+   */
+  public void execute();
+
+}
